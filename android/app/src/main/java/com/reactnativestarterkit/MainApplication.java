8f422492df640825f17b9fd83c30b57f6fc3625c
nimamoradi <ni_moradi@yahoo.com>
2019-04-21 22:44:16 +0430

    Initial commit

---
+++ android/app/src/main/java/com/reactnativestarterkit/MainApplication.java
@@ -0,0 +1,66 @@
+package com.reactnativestarterkit;
+
+import com.lugg.ReactNativeConfig.ReactNativeConfigPackage;
+
+import com.facebook.react.ReactNativeHost;
+import com.facebook.react.ReactPackage;
+
+
+import java.util.Arrays;
+import java.util.List;
+
+import com.reactnativenavigation.NavigationApplication;
+import com.reactnativenavigation.react.NavigationReactNativeHost;
+import com.reactnativenavigation.react.ReactGateway;
+
+import com.oblador.vectoricons.VectorIconsPackage;
+
+import com.facebook.CallbackManager;
+import com.facebook.appevents.AppEventsLogger;
+
+public class MainApplication extends NavigationApplication {
+
+    private static CallbackManager mCallbackManager = CallbackManager.Factory.create();
+
+    protected static CallbackManager getCallbackManager() {
+        return mCallbackManager;
+    }
+
+    @Override
+    protected ReactGateway createReactGateway() {
+        ReactNativeHost host = new NavigationReactNativeHost(this, isDebug(), createAdditionalReactPackages()) {
+            @Override
+            protected String getJSMainModuleName() {
+                return "index";
+            }
+        };
+        return new ReactGateway(this, isDebug(), host);
+    }
+
+    @Override
+    public boolean isDebug() {
+        return BuildConfig.DEBUG;
+    }
+
+    protected List<ReactPackage> getPackages() {
+        // Add additional packages you require here
+        // No need to add RnnPackage and MainReactPackage
+        return Arrays.<ReactPackage>asList(
+                new VectorIconsPackage(),
+                new ReactNativeConfigPackage()
+        );
+    }
+
+    @Override
+    public List<ReactPackage> createAdditionalReactPackages() {
+        return getPackages();
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        // Use AppEventsLogger to log custom events.
+        AppEventsLogger.activateApp(this);
+    }
+}
