3e91a954b9b61e54bd9224dde1de9fb1b35bf197
nimamoradi <ni_moradi@yahoo.com>
2019-05-09 14:26:41 +0430

    calendar ui

--- src/screens/calender/CalenderPage.js
+++ src/screens/calender/CalenderPage.js
@@ -43,15 +43,34 @@ class calenderPage extends PureComponent {
       <View
         style={styles.flex}>
         <Calendar
-          markedDates={{
-            '2019-5-25': {
-              selected: true,
-              selectedColor: 'red'
-            },
-            '2019-6-26': {
-              disabled: true
-            },
+          style={{
+            borderWidth: 1,
+            borderColor: 'gray',
+            height: 50 * vh,
+            width: 90 * vw,
+            borderRadius: 10 * vw,
+            elevation: 2 * vw
           }}
+          theme={{
+            textSectionTitleColor: '#b6c1cd',
+            selectedDayBackgroundColor: '#00adf5',
+            selectedDayTextColor: '#ffffff',
+            todayTextColor: 'green',
+            dayTextColor: '#2d4150',
+            textDisabledColor: '#d9e1e8',
+            dotColor: '#00adf5',
+            selectedDotColor: '#ffffff',
+            arrowColor: 'orange',
+            monthTextColor: '#000',
+            textDayFontFamily: 'monospace',
+            textMonthFontFamily: 'monospace',
+            textDayHeaderFontFamily: 'monospace',
+            textMonthFontWeight: '400',
+            textDayFontSize: 16,
+            textMonthFontSize: 16,
+            textDayHeaderFontSize: 16
+          }}
+
           markingType={'multi-dot'}
         />
 
